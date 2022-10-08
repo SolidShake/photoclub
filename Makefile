678248d3e0d8ed6fd64bb2e91cde1c7324a3b89b
Artem Utkin <solid.utkin@gmail.com>
2022-10-08 10:08:25 +0300

    add swagger

--- Makefile
+++ Makefile
@@ -1,5 +1,1 @@
-backend_start:
-	docker-compose -f backend/docker-compose.yml up -d
-
-backend_stop:
-	docker-compose -f backend/docker-compose.yml down
+include backend/Makefile
--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
-	"github.com/SolidShake/photoclub/app"
+	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/SolidShake/photoclub/docs"
+
+	apiAuth "github.com/SolidShake/photoclub/internal/api/auth"
 )
 
+// @title           Photoclub API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.apikey  ApiKeyAuth
+// @in                          header
+// @name                        Authorization
+// @description					Description for what is this security definition being used
+
 func main() {
-	app.Run()
+	r := gin.Default()
+
+	v1 := r.Group("/api/v1")
+	{
+		auth := v1.Group("/auth")
+		{
+			auth.POST("/register", apiAuth.Register)
+			auth.POST("/login", apiAuth.Login)
+		}
+	}
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8080")
 }
